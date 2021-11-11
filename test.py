8eb508baa7268f4200cb25da53c4dd6143d10c54
anshikageanra <aytr1024@gmail.com>
2021-11-11 11:44:34 -0500

    add sample files

--- test.py
+++ test.py
@@ -6,11 +6,24 @@ files = pd.read_csv('/users/anshikageanra/desktop/lab/data/log_sample.csv')
 new_path = '/Users/anshikageanra/PycharmProjects/ML-projects/sample/'
 
 file_path = files.iloc[:, 4]
-# file = file_path[0].split('/')
+# file = file_path[0].split('vcs/')
+# filename = file[-1].split('/')
+# dir = file[-1].split('/'+'{filename1}'.format(filename1=filename[-1]))
+# # print(dir)
+# newdir = os.path.join(new_path, dir[0])
+# print(newdir)
+# if not os.path.isdir(newdir):
+#     os.makedirs(newdir)
 # new = os.path.join(new_path,file[-1])
-# print(new)
+# # print(new)
 # print(file[-1])
+
 for i in file_path:
-    file = i.split('/')
+    file = i.split('vcs/')
+    filename = file[-1].split('/')
+    dir = file[-1].split('/'+'{filename1}'.format(filename1=filename[-1]))
+    newdir = os.path.join(new_path, dir[0])
+    if not os.path.isdir(newdir):
+        os.makedirs(newdir)
     new = os.path.join(new_path, file[-1])
     shutil.copyfile(i, new)
